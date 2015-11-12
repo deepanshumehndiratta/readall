fa9b42d9e28a2311cd34a4cdd73ef9b74d77e965
Deepanshu Mehndiratta <deepanshumehndiratta@gmail.com>
2015-11-12 21:27:00 +0530

    install dependencies from setup.py file

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='FlaskApp',
       author='Deepanshu Mehndiratta',
       author_email='contact@deepanshumehndiratta.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+     install_requires=['Flask>=0.10.1', 'flask-mail', 'readability-api', 'python-twitter'],
+
      )
